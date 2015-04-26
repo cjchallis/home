75de970276cf9f96a337cea363c65f8eb9b60b0b
cjchallis <christopher.challis@gmail.com>
2015-04-25 23:16:22 -0400

    First working version of install script

--- install.sh
+++ install.sh
@@ -2,3 +2,7 @@ cp -al bashrc ~/.bashrc
 cp -al vim ~/.vim
 cp -al vimrc ~/.vimrc
 cp -al solarized ~/.solarized
+
+# The following installs the solarized dark theme to the 2nd terminal profile
+~/.solarized/gnome-terminal-colors-solarized/install.sh -s "dark" -p "Default" 
+eval `dircolors ~/.solarized/dircolors-solarized/dircolors.256dark` 
