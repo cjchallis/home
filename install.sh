b316da5a715e6c53b81ccb8a68745092134947bc
cjchallis <christopher.challis@gmail.com>
2015-04-27 11:14:05 -0400

    Modify install.sh to clone required repositories

--- install.sh
+++ install.sh
@@ -1,8 +1,20 @@
+#!/bin/bash
 cp -al bashrc ~/.bashrc
-cp -al vim ~/.vim
 cp -al vimrc ~/.vimrc
+
+cd vim/bundle
+git clone https://github.com/altercation/vim-colors-solarized.git
+cd ..
+cd ..
+rm -rf ~/.vim
+cp -al vim ~/.vim
+
+mkdir solarized
+cd solarized
+git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git
+git clone https://github.com/seebi/dircolors-solarized.git
+cd ..
+rm -rf ~/.solarized
 cp -al solarized ~/.solarized
 
-# The following installs the solarized dark theme to the 2nd terminal profile
 ~/.solarized/gnome-terminal-colors-solarized/install.sh -s "dark" -p "Default" 
-eval `dircolors ~/.solarized/dircolors-solarized/dircolors.256dark` 
