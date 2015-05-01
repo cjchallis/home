f0726b06f93f90d7af4bbce96eb3e83bbf7856c4
cjchallis <christopher.challis@gmail.com>
2015-05-01 00:30:50 -0400

    Change from cp -al to ln -f for individual files

--- install.sh
+++ install.sh
@@ -9,32 +9,31 @@ echo "  ~/.git-ps1.sh"
 echo "  ~/.vim/"
 echo "  ~/.solarized"
 echo
-echo "Would you like to continue? (y/n)"
+echo "Would you like to continue? (y/N)"
 read yesno
 
-if [ $yesno != y ]; then
+if [[ $yesno != y ]]; then
     echo "Confirmation failed - aborting."
     exit 1; 
 fi
 
 echo "Linking home directory hidden files."
-cp -al bashrc ~/.bashrc
-cp -al vimrc ~/.vimrc
-cp -al colors ~/.colors
-cp -al git-ps1.sh ~/.git-ps1.sh
+ln -f bashrc ~/.bashrc
+ln -f vimrc ~/.vimrc
+ln -f colors ~/.colors
+ln -f git-ps1.sh ~/.git-ps1.sh
 
-if [ ! -d vim/bundle/vim-colors-solarized ]; then
+if [[ ! -d vim/bundle/vim-colors-solarized ]]; then
     cd vim/bundle
     git clone https://github.com/altercation/vim-colors-solarized.git
-    cd ..
-    cd ..
+    cd ../..
 fi
 
 echo "Overwriting ~/.vim/"
 rm -rf ~/.vim
 cp -al vim ~/.vim
 
-if [ ! -d solarized ]; then
+if [[ ! -d solarized ]]; then
     mkdir solarized
     cd solarized
     git clone https://github.com/Anthony25/gnome-terminal-colors-solarized.git
@@ -42,16 +41,16 @@ if [ ! -d solarized ]; then
     cd ..
 fi
 
-echo "Overwriting ~/.solarized"
+echo "Overwriting ~/.solarized/"
 rm -rf ~/.solarized
 cp -al solarized ~/.solarized
 
-if [ $(dpkg-query -W -f='${Status}' dconf-cli 2>/dev/null | grep -c "ok installed") -eq 0 ];
+if [[ $(dpkg-query -W -f='${Status}' dconf-cli 2>/dev/null | grep -c "ok installed") -eq 0 ]];
 then
     apt-get install dconf-cli;
 fi
 
 echo "Changing terminal theme colors."
 ~/.solarized/gnome-terminal-colors-solarized/install.sh -s "dark" -p "Default" 
-echo "Installation succesful. Most directory settings other will take effect"
+echo "Installation succesful. Most directory settings will take effect"
 echo "in new terminal."
